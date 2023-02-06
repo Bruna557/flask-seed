13576bb285b2bbdd0cc8960685fd1a83bbfa4975
bruna.schroder <bruna.schroder@hp.com>
2023-02-06 17:32:22 -0300

    Clean up conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,8 @@
-# pylint: disable=redefined-outer-name
-import time
-from pathlib import Path
-
 import pytest
-import requests
-from requests.exceptions import ConnectionError
-from sqlalchemy.exc import OperationalError
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers
 
 from users.adapters.orm import metadata, start_mappers
-from users import config
 
 
 @pytest.fixture
